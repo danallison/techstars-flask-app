0f83fd54dc5c7c61a95e9ccca043b17a363764a6
Dan Allison <daniel.j.allison@gmail.com>
2021-11-19 11:07:49 -0800

    Create DB, REST API, and tests

--- README.md
+++ README.md
@@ -2,15 +2,48 @@
 
 > Using Python and Flask, write an API that exposes an endpoint that (at a minimum) returns a 200 status.  Then dockerize the solution so it runs in an image or from a docker-compose file.
 
+# Description
+
+This Flask app provides a simple REST API for creating and reading records from the `records` table in the SQLite DB `database.db`. Records are simple example data with a `text` attribute and an automatically generated `id` attribute.
+
+# Dev Setup
+
+To run the app in your local dev environment, use `docker-compose up` to start the `flask_app` container running on port 5000 and the `jupyter` container running on port 8888. To use Jupyter, use the passcode specified in the `docker-compose.yml` file environment variable `JUPYTER_TOKEN`.
+
+In Jupyter, use the `db_setup` notebook to setup the database. Be aware that running the code in this notebook will drop any existing data in the `records` table. So be careful if you have any data that you do not want to lose.
+
+Use the `test` notebook to test out the Flask app API.
+
 # API Documentation
 
 ## Routes
 
 ### `GET /`
 
-Returns JSON
-```
-{
-  "message": "Hello World!"
-}
-```
+Returns JSON object that describes available routes, expected input parameters, and response format.
+
+### `POST /records`
+
+Creates a new record and returns that record object with its ID.
+
+Required parameter is `text` attribute, which is a string.
+
+### `GET /records`
+
+Returns complete list of all records.
+
+### `GET /records/<id>`
+
+Returns single record with ID `id`, if it exists.
+
+# Possible Future Improvements
+
+- Error handling
+- Input validation
+- Pagination of records list
+- User authentication
+- Automated testing
+- Logging
+- Deployment
+- CI/CD
+- `PUT` and `DELETE` routes
--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,68 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
+
+# Helper function
+def record_as_dict(record):
+    return {
+        'id': record[0],
+        'text': record[1]
+    }
 
 @app.route('/')
 def root():
     return jsonify({
-        'message': 'Hello World!'
+        'routes': {
+            '/': {
+                'GET': {
+                    'params': None,
+                    'returns': 'json description of API routes'
+                }
+            },
+            '/records': {
+                'POST': {
+                    'params': { 'text': '[string]' },
+                    'returns': 'record object with ID'
+                },
+                'GET': {
+                    'params': None,
+                    'returns': 'list of all records'
+                }
+            },
+            '/records/<id>': {
+                'GET': {
+                    'params': None,
+                    'returns': 'record object with matching ID, if it exists'
+                }
+            }
+        }
     })
+
+@app.route('/records', methods=['GET'])
+def list():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM records")
+        records = cur.fetchall()
+    return jsonify([record_as_dict(record) for record in records])
+
+@app.route('/records/<id>', methods=['GET'])
+def show(id):
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM records WHERE id = ? LIMIT 1", (id,))
+        record = cur.fetchone()
+    return jsonify(record_as_dict(record))
+
+@app.route('/records', methods=['POST'])
+def create():
+    data = request.get_json(force=True)
+    text = data['text']
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO records (text) VALUES (?)", (text,))
+        conn.commit()
+        cur.execute("SELECT * FROM records WHERE id = ? LIMIT 1", (cur.lastrowid,))
+        record = cur.fetchone()
+    return jsonify(record_as_dict(record))
